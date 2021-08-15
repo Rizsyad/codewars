142f8880eafea8bf2debeeaf093fcade748f9b32
Rizsyad AR <42699914+Rizsyad@users.noreply.github.com>
2021-08-16 00:01:16 +0700

    adding kata solutions

---
+++ JavaScript/7kyu/Basic Calculator/main.js
@@ -0,0 +1,11 @@
+const calculate = (num1, operation, num2) => (!(["+","-","*","/"].includes(operation)) || (operation === '/' && num2 === 0)) ? null : eval(`${num1}${operation}${num2}`)
+
+console.log(calculate(3.2,"+", 8), 11.2, "3.2 + 8 = 11.2")
+console.log(calculate(3.2,"-", 8), -4.8, "3.2 - 8 = -4.8")
+console.log(calculate(3.2,"/", 8), 0.4, "3.2 / 8 = .4")
+console.log(calculate(3.2,"*", 8), 25.6, "3.2 * 8 = 25.6")
+console.log(calculate(-3,"+", 0), -3, "-3 + 0 = -3")
+console.log(calculate(-3,"-", 0), -3, "-3 - 0 = -3")
+console.log(calculate(-3,"/", 0), null, "-3 / 0 = null")
+console.log(calculate(-3,"*", 0), 0, "-3 * 0 = 0")
+console.log(calculate(-3,"w", 0), null, "-3 w 0 = null")
